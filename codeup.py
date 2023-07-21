5984a21adf5fe6198a1b298e89b2d02c7be1dbc0
hyemi1129 <kimhyemi1243@gmail.com>
2023-07-21 09:58:28 +0900

    1st commit

---
+++ codeup.py
@@ -0,0 +1,31 @@
+"""
+6025
+ a, b = input().split()
+c = int(a) + int(b)
+print(c) """
+
+"""
+6026
+ a = input()
+b = input()
+c = float(a) + float(b)
+print(c) """
+
+""" 6027
+a= input()
+n = int(a)
+print('%x'% n) """
+
+""" 6028
+a= input()
+n = int(a)
+print('%X'% n) """
+
+""" 6029
+a= input()
+n = int(a, 16)
+print('%o'% n) """
+
+
+a = ord(input())
+print(a)

4b7a11dc60f17aacd6afd764eccbfd44ee15683b
hyemi1129 <kimhyemi1243@gmail.com>
2023-07-22 14:32:02 +0900

    3rd commit

--- codeup.py
+++ codeup.py
@@ -30,6 +30,205 @@ print('%o'% n) """
 a = ord(input())
 print(a) """
 
-
+""" 6031
 c = int(input())
-print(chr(c))
+print(chr(c)) """
+
+""" 6032
+a = input()
+a = int(a)
+print(-a) """
+
+""" 6033
+a = ord(input())
+print(chr(a+1)) """
+
+""" 6034
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a-b)
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+c = a - b
+print(c) """
+
+""" 6035
+a, b = input().split()
+a = float(a)
+b = float(b)
+print(a*b)
+
+a, b = input().split()
+a = float(a)
+b = float(b)
+c = a * b
+print(c) """
+
+""" 6036
+a, b = input().split()
+print(a*int(b))
+
+a, b = input().split()
+b = int(b)
+print(a*b) """
+
+""" 6037
+a = input()
+b = input()
+print(int(a)*b)
+
+a = input()
+b = input()
+a = int(a)
+print(a*b) """
+
+""" 6038
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a**b)
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+c = a**b
+print(c) """
+
+""" 6039
+a, b = input().split()
+a = float(a)
+b = float(b)
+print(a**b)
+
+a, b = input().split()
+a = float(a)
+b = float(b)
+c = a**b
+print(c) """
+
+""" 6040
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a//b) """
+
+""" 6041
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a%b) """
+
+""" 6042
+a = input()
+a = float(a)
+print(format(a, ".2f")) """
+
+""" 6043
+a, b = input().split()
+a = float(a)
+b = float(b)
+print(format(a/b, ".3f")) """
+
+""" 6044
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(format(a/b, ".2f")) """
+
+""" 6045
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+print(a+b+c, format((a+b+c)/3, ".2f")) """
+
+""" 6046
+a = input()
+a = int(a)
+print(a*2)
+
+a = input()
+a = int(a)
+print(a<<1) """
+
+""" 6047
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a*(2**b)) """
+
+""" 6048
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a<b) """
+
+""" 6049
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a==b) """
+
+""" 6050
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(b>=a) """
+
+""" 6051
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a!=b) """
+
+""" 6052
+a = int(input())
+print(bool(a)) """
+
+""" 6053
+a = bool(int(input()))
+print(not a) """
+
+""" 6054
+a, b = input().split()
+print(bool(int(a)) and bool(int(b))) """
+
+""" 6055
+a, b = input().split()
+print(bool(int(a)) or bool(int(b))) """
+
+""" 6056
+a, b = input().split()
+c = bool(int(a))
+d = bool(int(b))
+print((c and (not d)) or ((not c) and d)) """
+
+""" 6057
+a, b = input().split()
+a = bool(int(a))
+b = bool(int(b))
+print(a==b) """
+
+""" 6058
+a, b = input().split()
+a = bool(int(a))
+b = bool(int(b))
+print(not(a or b)) """
+
+""" 6059
+a = int(input())
+print(~a) """
+
+""" 6060
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a&b) """

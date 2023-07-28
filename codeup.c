d353a4e834b620be66e5cac274a4c70190bce6d5
hyemi1129 <kimhyemi1243@gmail.com>
2023-07-28 19:22:19 +0900

    1st commit

--- codeup.c
+++ codeup.c
@@ -1,5 +1,12 @@
-#include <stdio.h>
+/* #include <stdio.h>
 int main()
 {
-    printf("Hello World");
-}
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a%2==0)
+    printf("%d\n", a);
+    if(b%2==0)
+    printf("%d\n", b);
+    if(c%2==0)
+    printf("%d", c);
+} */
